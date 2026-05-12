815445bceb93ef7eb2d64a9b91702da63a836a64
AsokTamang <ashoktmg2058@gmail.com>
2026-05-12 12:22:35 -0400

    load data file created

--- src/logger.py
+++ src/logger.py
@@ -3,6 +3,6 @@ from pathlib import Path
 import logging
 
 dir_path =Path(__file__).parent
-filepath  = os.path.join(dir_path, 'loggings.log')
+filepath  = os.path.join(dir_path, 'loggings.log')  #all the logs will go inside this file 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',filename=filepath)
 
