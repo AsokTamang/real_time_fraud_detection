e6dd783c73857ac789324a80fcb42aea3049ea7b
AsokTamang <ashoktmg2058@gmail.com>
2026-05-12 23:45:42 -0400

    main file

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import sys
+import os
 sys.path.append('.')
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+data_path    = os.path.join(BASE_DIR, 'data', 'pay_sim.csv')
+model_path   = os.path.join(BASE_DIR, 'artifacts', 'model.pkl')
