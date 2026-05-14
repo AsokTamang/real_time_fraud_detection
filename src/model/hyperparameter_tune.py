d2782952026a4a6e2802dd44a603c44d3a22907b
AsokTamang <ashoktmg2058@gmail.com>
2026-05-14 14:34:56 -0400

    hyperparameter tuned script

--- src/model/hyperparameter_tune.py
+++ src/model/hyperparameter_tune.py
@@ -2,7 +2,7 @@ from lightgbm import LGBMClassifier
 import optuna
 from optuna.samplers import TPESampler
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import StratifiedKFold, cross_val_score
+from sklearn.model_selection import TimeSeriesSplit, cross_val_score
 from xgboost import XGBClassifier
 optuna.logging.set_verbosity(optuna.logging.WARNING)  # ✅ cleaner output
 
@@ -27,11 +27,9 @@ class EarlyStoppingCallback:
 
 
 def optimize_model_cv(X_train,X_val,X_test,y_train,y_val,y_test, models,n_trials=5):
-    cv = StratifiedKFold(n_splits=3, shuffle=True, random_state=42)
-    best_overall_model= None
-    best_overall_score  = -1
-    best_overall_preds  = 0
-    best_overall_name   = None
+    SEED =42
+    cv = TimeSeriesSplit(n_splits=5, shuffle=True, random_state=SEED)
+    results = {}
 
     for model_name in models:
 
@@ -44,7 +42,7 @@ def optimize_model_cv(X_train,X_val,X_test,y_train,y_val,y_test, models,n_trials
                     'min_samples_leaf' : trial.suggest_int('min_samples_leaf', 1, 5),
                     'max_features'     : trial.suggest_categorical('max_features', ['sqrt', 'log2']),
                     'class_weight'     : 'balanced',
-                    'random_state'     : 42,
+                    'random_state'     : SEED,
                     'n_jobs'           : -1,    
                 }
                 model = RandomForestClassifier(**params)
@@ -58,7 +56,7 @@ def optimize_model_cv(X_train,X_val,X_test,y_train,y_val,y_test, models,n_trials
                     'colsample_bytree': trial.suggest_float('colsample_bytree', 0.6, 1.0),
                     'min_child_weight': trial.suggest_int('min_child_weight', 1, 10),
                     'scale_pos_weight': len(y_train[y_train==0]) / len(y_train[y_train==1]),
-                    'random_state'    : 42,
+                    'random_state'    : SEED,
                     'eval_metric'     : 'aucpr',
                     'device'          : 'cuda',
                     'tree_method'     : 'hist'
@@ -77,13 +75,13 @@ def optimize_model_cv(X_train,X_val,X_test,y_train,y_val,y_test, models,n_trials
                 'min_child_samples': trial.suggest_int('min_child_samples', 2, 20),
                 'min_child_weight' : 0.001, # Helps prevent the 'left_count > 0' error
                 'scale_pos_weight' : len(y_train[y_train==0]) / len(y_train[y_train==1]),
-                'random_state'     : 42,
+                'random_state'     : SEED,
                 'device'           : 'cpu', # Changed to CPU for stability
                 'verbose'          : -1,
                 }
                 model = LGBMClassifier(**params)
 
-            n_jobs = -1 if model_name == 'Random Forest' else 1
+            n_jobs = -1 if model_name in ['Random Forest','LightGBM'] else 1
             
             scores = cross_val_score(
                 model, X_train, y_train,
@@ -103,28 +101,16 @@ def optimize_model_cv(X_train,X_val,X_test,y_train,y_val,y_test, models,n_trials
         study.optimize(
             objective,
             n_trials=n_trials,
-            n_jobs=1,                       # ✅ sequential for GPU safety
+            n_jobs=1,                      
             callbacks=[early_stop]
         )
+        results[model_name] = study.best_params  #here we are storing the best parameter of the model with their best parameters
+    return results
 
-        print(f'{model_name} — Best CV PR-AUC: {study.best_value:.4f}')
-        print(f'Best Params: {study.best_params}')
-        best_params = study.best_params
-        model_map = {
-            'Random Forest': RandomForestClassifier(**best_params, class_weight='balanced', random_state=42, n_jobs=-1),
-            'XGBoost'      : XGBClassifier(**best_params, random_state=42, eval_metric='aucpr', device='cuda', tree_method='hist'),
-            'LightGBM'     : LGBMClassifier(**best_params, random_state=42, device='cpu'),
-        }
-        final_model = model_map[model_name]
-        final_model.fit(X_train, y_train)  #training the model on training dataset
-        preds = final_model.predict_proba(X_test)[:,1] #final prediction done on test dataset
 
-        if study.best_value > best_overall_score:
-            best_overall_score  = study.best_value  #storing the latest best cross_validation score
-            best_overall_model  = final_model
-            best_overall_preds  = preds
-            best_overall_name   = model_name
 
-    print(f'\n✅ Best Overall Model : {best_overall_name}')
-    print(f'✅ Best CV PR-AUC     : {best_overall_score:.4f}')
-    return best_overall_model, best_overall_score, best_overall_preds
+
+
+
+
+        
--- src/model/train_pipeline.py
+++ src/model/train_pipeline.py
@@ -1,22 +1,38 @@
+import os
+import sys
+from dataclasses import dataclass
 from lightgbm import LGBMClassifier
-from sklearn.metrics import (classification_report, roc_auc_score,
-                             average_precision_score, precision_recall_curve,
-                             recall_score, precision_score, f1_score)
+from src.utils import save_object
+from src.exception import CustomError
+from src.logger import logging
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from xgboost import XGBClassifier
-from tune_pipeline import optimize_model_cv
+from src.model.hyperparameter_tune import optimize_model_cv
 
 
-def train_model(X_train,X_val,X_test,y_train,y_val,y_test):
-    models = {
-    'LightGBM': LGBMClassifier(verbose=-1, class_weight='balanced',
-                                    n_estimators=100, random_state=42),
-    'XGBoost': XGBClassifier(scale_pos_weight=len(y_train[y_train==0])/len(y_train[y_train==1]),    #here we are using scale_pos_weight to handle the class imbalance in the dataset by balanced weight  to both classes based on their ratio, which helps the model to effectively classifying the fraudulent transactions.
-                                n_estimators=100, random_state=42, eval_metric='aucpr'),
-    'Random Forest': RandomForestClassifier(class_weight='balanced', n_estimators=100,
-                                                random_state=42, n_jobs=-1)
-    }
-    best_model,best_model_score,test_prediction = optimize_model_cv(X_train,X_val,X_test,y_train,y_val,y_test, models,n_trials=5)
+@dataclass
+class ModelTrainerConfig:
+    trained_model_path:str = os.path.join('artifacts','model.pkl')
+
+
+class ModelTrainer:
+    def __init__(self):
+          self.model_trainer_config = ModelTrainerConfig()
+
+    def train_model(self,X_train,X_val,X_test,y_train,y_val,y_test):
+        neg = (y_train == 0).sum()
+        pos = (y_train == 1).sum()
+        ratio = neg/pos
+        models = {
+        'LightGBM': LGBMClassifier(verbose=-1, class_weight='balanced',
+                                        n_estimators=100, random_state=42),
+        'XGBoost': XGBClassifier(scale_pos_weight=ratio,    #here we are using scale_pos_weight to handle the class imbalance in the dataset by balanced weight  to both classes based on their ratio, which helps the model to effectively classifying the fraudulent transactions.
+                                    n_estimators=100, random_state=42, eval_metric='aucpr'),
+        'Random Forest': RandomForestClassifier(class_weight='balanced', n_estimators=100,
+                                                    random_state=42, n_jobs=-1)
+        }
+        hyperparameter_tuned_models= optimize_model_cv(X_train,X_val,X_test,y_train,y_val,y_test, models,n_trials=25)
+        
         
