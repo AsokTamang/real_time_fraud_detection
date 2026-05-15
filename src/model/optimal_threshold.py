7372c9261b04fbe9c694b801a5a748a4fdb31eb2
AsokTamang <ashoktmg2058@gmail.com>
2026-05-15 13:50:35 -0400

    mlflow integration in hyperparamter tuning

--- src/model/optimal_threshold.py
+++ src/model/optimal_threshold.py
@@ -1,5 +1,6 @@
 from src.utils import save_object
-
+import mlflow
+import mlflow.sklearn
 from lightgbm import LGBMClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import (
@@ -19,14 +20,18 @@ class initiate_threshold_tuning:
     ):
         neg = (y_train == 0).sum()
         pos = (y_train == 1).sum()
+
         thresholds = {}
         fitted_model = {}
+
         SEED = 42
+
         rf_fixed_params = {
             "random_state": SEED,
             "n_jobs": -1,
             "class_weight": "balanced",
         }
+
         XG_fixed_params = {
             "scale_pos_weight": neg / pos,
             "random_state": SEED,
@@ -35,32 +40,78 @@ class initiate_threshold_tuning:
             "tree_method": "hist",
             "early_stopping_rounds": 15,
         }
+
         LGM_fixed_params = {
             "scale_pos_weight": neg / pos,
             "random_state": SEED,
             "n_jobs": -1,
         }
 
+        mlflow.set_experiment("threshold_tuning_experiment")
+
         for name, best_params in hyperparameter_tuned_models.items():
-            if name == "Random Forest":
-                model = RandomForestClassifier(**best_params, **rf_fixed_params)
-            elif name == "XGBoost":
-                model = XGBClassifier(**best_params, **XG_fixed_params)
-            else:
-                model = LGBMClassifier(**best_params, **LGM_fixed_params)
 
-            model.fit(X_train, y_train)  # fitting the model on the training data
-            y_prob = model.predict_proba(X_val)[
-                :, 1
-            ]  # only extracting the probability of the positive class (fraud)
+            with mlflow.start_run(run_name=name):
 
-            fitted_model[name] = model
-            print(f"saved the trained model {name}")
+                # -------------------------
+                # Model selection
+                # -------------------------
+                if name == "Random Forest":
+                    model = RandomForestClassifier(**best_params, **rf_fixed_params)
+                elif name == "XGBoost":
+                    model = XGBClassifier(**best_params, **XG_fixed_params)
+                else:
+                    model = LGBMClassifier(**best_params, **LGM_fixed_params)
 
-            precisions, recalls, thresh = precision_recall_curve(y_val, y_prob)
-            fbeta = (1 + 4) * (precisions * recalls) / (4 * precisions + recalls + 1e-8)
-            best_idx = fbeta.argmax()
-            thresholds[name] = thresh[best_idx]
-            print(f"Best threshold: {thresh[best_idx]:.4f}")
+                # -------------------------
+                # Training model
+                # -------------------------
+                model.fit(X_train, y_train)
+                y_prob = model.predict_proba(X_val)[:, 1]
 
-        return fitted_model,thresholds
+                fitted_model[name] = model
+                print(f"trained model {name}")
+
+                # -------------------------
+                # Metrics
+                # -------------------------
+                pr_auc = average_precision_score(y_val, y_prob)
+                roc_auc = roc_auc_score(y_val, y_prob)
+
+                precisions, recalls, thresh = precision_recall_curve(y_val, y_prob)
+
+                fbeta = (1 + 4) * (precisions * recalls) / (4 * precisions + recalls + 1e-8)
+                best_idx = fbeta.argmax()
+                best_threshold = thresh[best_idx]
+
+                thresholds[name] = best_threshold
+
+                # apply threshold
+                y_pred = (y_prob >= best_threshold).astype(int)
+
+                f1 = f1_score(y_val, y_pred)
+                precision = precision_score(y_val, y_pred)
+                recall = recall_score(y_val, y_pred)
+
+                # -------------------------
+                # MLflow logging
+                # -------------------------
+                mlflow.log_params(best_params)
+                mlflow.log_param("model_name", name)
+                mlflow.log_param("best_threshold", best_threshold)
+
+                mlflow.log_metric("pr_auc", pr_auc)
+                mlflow.log_metric("roc_auc", roc_auc)
+                mlflow.log_metric("f1_score", f1)
+                mlflow.log_metric("precision", precision)
+                mlflow.log_metric("recall", recall)
+
+                # loggin model
+                if name == 'XGBoost':
+                    mlflow.xgboost.log_model(model,artifact_path="model")
+                else:    
+                    mlflow.sklearn.log_model(model, artifact_path="model")
+
+                print(f"best threshold for {name}: {best_threshold:.4f}")
+
+        return fitted_model, thresholds
