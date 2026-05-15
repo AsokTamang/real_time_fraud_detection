4a09e992471d9a3748d2c2945c7316c1eda4e351
AsokTamang <ashoktmg2058@gmail.com>
2026-05-15 13:53:59 -0400

    mlflow integration in optimal threshold tuning

--- src/model/hyperparameter_tune.py
+++ src/model/hyperparameter_tune.py
@@ -7,106 +7,154 @@ from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
 import numpy as np
 from lightgbm import early_stopping
+import mlflow
 
 optuna.logging.set_verbosity(optuna.logging.WARNING)
 
-SEED = 42  
+SEED = 42
 
-def optimize_model_cv(X_train, X_val, X_test, y_train, y_val, y_test, models, n_trials=25):
+
+def optimize_model_cv(
+    X_train, X_val, X_test, y_train, y_val, y_test, models, n_trials=25
+):
     cv = TimeSeriesSplit(n_splits=5)
     hyperparameter_tuned_models = {}
-
+    mlflow.set_experiment("Optuna Hyperparameter Tuning")
     for model_name in models:
-        def objective(trial, model_name=model_name):  
-            fold_scores = []
+        with mlflow.start_run(run_name=model_name):
 
-            for fold, (train_idx, val_idx) in enumerate(cv.split(X_train)):
-                X_tr = X_train.iloc[train_idx]
-                X_fold_val = X_train.iloc[val_idx]
-                y_tr = y_train.iloc[train_idx]
-                y_fold_val = y_train.iloc[val_idx]
+            def objective(trial, model_name=model_name):
+                fold_scores = []
 
-                neg = (y_tr == 0).sum()
-                pos = (y_tr == 1).sum()
+                for fold, (train_idx, val_idx) in enumerate(cv.split(X_train)):
+                    X_tr = X_train.iloc[train_idx]
+                    X_fold_val = X_train.iloc[val_idx]
+                    y_tr = y_train.iloc[train_idx]
+                    y_fold_val = y_train.iloc[val_idx]
 
-                if model_name == 'Random Forest':
-                    params = {
-                        'n_estimators': trial.suggest_int('n_estimators', 50, 150),
-                        'max_depth': trial.suggest_int('max_depth', 3, 7),
-                        'min_samples_split': trial.suggest_int('min_samples_split', 2, 6),
-                        'min_samples_leaf': trial.suggest_int('min_samples_leaf', 1, 5),
-                        'max_features': trial.suggest_categorical('max_features', ['sqrt', 'log2']),
-                        #  Fixed params included here
-                        'class_weight': 'balanced',
-                        'random_state': SEED,
-                        'n_jobs': -1,
-                    }
-                    model = RandomForestClassifier(**params)
-                    model.fit(X_tr, y_tr)
+                    neg = (y_tr == 0).sum()
+                    pos = (y_tr == 1).sum()
 
-                elif model_name == 'XGBoost':
-                    # Conditional CUDA
-                    try:
-                        import subprocess
-                        subprocess.check_output('nvidia-smi')
-                        device = 'cuda'
-                    except Exception:
-                        device = 'cpu'
+                    if model_name == "Random Forest":
+                        params = {
+                            "n_estimators": trial.suggest_int("n_estimators", 50, 150),
+                            "max_depth": trial.suggest_int("max_depth", 3, 7),
+                            "min_samples_split": trial.suggest_int(
+                                "min_samples_split", 2, 6
+                            ),
+                            "min_samples_leaf": trial.suggest_int(
+                                "min_samples_leaf", 1, 5
+                            ),
+                            "max_features": trial.suggest_categorical(
+                                "max_features", ["sqrt", "log2"]
+                            ),
+                            #  Fixed params included here
+                            "class_weight": "balanced",
+                            "random_state": SEED,
+                            "n_jobs": -1,
+                        }
+                        model = RandomForestClassifier(**params)
+                        model.fit(X_tr, y_tr)
 
-                    params = {
-                        'n_estimators': trial.suggest_int('n_estimators', 100, 300),
-                        'max_depth': trial.suggest_int('max_depth', 3, 10),
-                        'learning_rate': trial.suggest_float('learning_rate', 0.01, 0.3, log=True),
-                        'subsample': trial.suggest_float('subsample', 0.6, 1.0),
-                        'colsample_bytree': trial.suggest_float('colsample_bytree', 0.6, 1.0),
-                        'min_child_weight': trial.suggest_int('min_child_weight', 1, 10),
-                        #  Fixed params 
-                        'scale_pos_weight': neg / pos,
-                        'random_state': SEED,
-                        'eval_metric': 'aucpr',
-                        'device': device,
-                        'tree_method': 'hist',
-                        'early_stopping_rounds': 15,
-                    }
-                    model = XGBClassifier(**params)
-                    model.fit(X_tr, y_tr, eval_set=[(X_fold_val, y_fold_val)], verbose=False)
+                    elif model_name == "XGBoost":
+                        # Conditional CUDA
+                        try:
+                            import subprocess
 
-                elif model_name == 'LightGBM':
-                    params = {
-                        'n_estimators': trial.suggest_int('n_estimators', 100, 300),
-                        'max_depth': trial.suggest_int('max_depth', 3, 7),
-                        'learning_rate': trial.suggest_float('learning_rate', 0.01, 0.3, log=True),
-                        'subsample': trial.suggest_float('subsample', 0.6, 1.0),
-                        'colsample_bytree': trial.suggest_float('colsample_bytree', 0.6, 1.0),
-                        'min_child_samples': trial.suggest_int('min_child_samples', 2, 20),
-                        'num_leaves': trial.suggest_int('num_leaves', 20, 50),
-                        # Fixed params included here
-                        'scale_pos_weight': neg / pos,
-                        'random_state': SEED,
-                        'n_jobs': -1,
-                    }
-                    model = LGBMClassifier(**params)
-                    model.fit(X_tr, y_tr, eval_set=[(X_fold_val, y_fold_val)],
-                              callbacks=[early_stopping(stopping_rounds=15, verbose=False)])
+                            subprocess.check_output("nvidia-smi")
+                            device = "cuda"
+                        except Exception:
+                            device = "cpu"
 
-                y_pred_proba = model.predict_proba(X_fold_val)[:, 1]
-                score = average_precision_score(y_fold_val, y_pred_proba)
-                fold_scores.append(score)
+                        params = {
+                            "n_estimators": trial.suggest_int("n_estimators", 100, 300),
+                            "max_depth": trial.suggest_int("max_depth", 3, 10),
+                            "learning_rate": trial.suggest_float(
+                                "learning_rate", 0.01, 0.3, log=True
+                            ),
+                            "subsample": trial.suggest_float("subsample", 0.6, 1.0),
+                            "colsample_bytree": trial.suggest_float(
+                                "colsample_bytree", 0.6, 1.0
+                            ),
+                            "min_child_weight": trial.suggest_int(
+                                "min_child_weight", 1, 10
+                            ),
+                            #  Fixed params
+                            "scale_pos_weight": neg / pos,
+                            "random_state": SEED,
+                            "eval_metric": "aucpr",
+                            "device": device,
+                            "tree_method": "hist",
+                            "early_stopping_rounds": 15,
+                        }
+                        model = XGBClassifier(**params)
+                        model.fit(
+                            X_tr,
+                            y_tr,
+                            eval_set=[(X_fold_val, y_fold_val)],
+                            verbose=False,
+                        )
 
-                trial.report(np.mean(fold_scores), fold)
-                if trial.should_prune():
-                    raise optuna.TrialPruned()
+                    elif model_name == "LightGBM":
+                        params = {
+                            "n_estimators": trial.suggest_int("n_estimators", 100, 300),
+                            "max_depth": trial.suggest_int("max_depth", 3, 7),
+                            "learning_rate": trial.suggest_float(
+                                "learning_rate", 0.01, 0.3, log=True
+                            ),
+                            "subsample": trial.suggest_float("subsample", 0.6, 1.0),
+                            "colsample_bytree": trial.suggest_float(
+                                "colsample_bytree", 0.6, 1.0
+                            ),
+                            "min_child_samples": trial.suggest_int(
+                                "min_child_samples", 2, 20
+                            ),
+                            "num_leaves": trial.suggest_int("num_leaves", 20, 50),
+                            # Fixed params included here
+                            "scale_pos_weight": neg / pos,
+                            "random_state": SEED,
+                            "n_jobs": -1,
+                        }
+                        model = LGBMClassifier(**params)
+                        model.fit(
+                            X_tr,
+                            y_tr,
+                            eval_set=[(X_fold_val, y_fold_val)],
+                            callbacks=[
+                                early_stopping(stopping_rounds=15, verbose=False)
+                            ],
+                        )
 
-            return np.mean(fold_scores)
+                    y_pred_proba = model.predict_proba(X_fold_val)[:, 1]
+                    score = average_precision_score(y_fold_val, y_pred_proba)
+                    fold_scores.append(score)
 
-        study = optuna.create_study(
-            direction='maximize',
-            sampler=TPESampler(seed=SEED),
-            pruner=optuna.pruners.MedianPruner(n_startup_trials=5, n_warmup_steps=2)  
-        )
-        study.optimize(objective, n_trials=n_trials, n_jobs=1)
+                    trial.report(np.mean(fold_scores), fold)
+                    if trial.should_prune():
+                        raise optuna.TrialPruned()
 
-        hyperparameter_tuned_models[model_name] = study.best_params
-        print(f"{model_name} | Best PR-AUC: {study.best_value:.4f} | Params: {study.best_params}")
+                return np.mean(fold_scores)
+
+            study = optuna.create_study(
+                direction="maximize",
+                sampler=TPESampler(seed=SEED),
+                pruner=optuna.pruners.MedianPruner(
+                    n_startup_trials=5, n_warmup_steps=2
+                ),
+            )
+            study.optimize(objective, n_trials=n_trials, n_jobs=1)
+
+            hyperparameter_tuned_models[model_name] = study.best_params
+            print(
+                f"{model_name} | Best PR-AUC: {study.best_value:.4f} | Params: {study.best_params}"
+            )
+            # -------------------------
+            # MLflow logging
+            # -------------------------
+            mlflow.log_param("model_name", model_name)
+            mlflow.log_metric("best_pr_auc", study.best_value)
+            mlflow.log_param("n_trials", n_trials)
+
+            mlflow.log_dict(study.best_params, "best_params.json")
 
     return X_train, X_val, X_test, y_train, y_val, y_test, hyperparameter_tuned_models
