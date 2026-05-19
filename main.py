d11f8615316ac1cd7f8d56c1c24bddf99e030fc5
AsokTamang <ashoktmg2058@gmail.com>
2026-05-19 13:09:09 -0400

    endpoint for fraud detection created

--- main.py
+++ main.py
@@ -1,14 +1,60 @@
+from src.exception import CustomError
+from src.logger import logging
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from src.pipeline.predict_pipeline import PredictPipeline
+from src.pipeline.data_validation_pipeline import CustomData
+from src.pipeline.feature_engineering_pipeline import Feature_engineering
+from pydantic import BaseModel
+from typing import Union
+import uvicorn
 import sys
-import os
-sys.path.append('.')
-from src.utils import load_object
+
+app = FastAPI()
+app.mount('/static', StaticFiles(directory='statics'), name='statics')
+
+feature_engineering_pipeline = Feature_engineering()
+predict_pipeline = PredictPipeline()  #calling the predict pipeline class
+
+@app.get('/')
+def root():
+    return FileResponse('statics/home.html')
+
+
+class PredictRequest(BaseModel):  #class for defining the input data for prediction
+    type: Union[str, None] = None,
+    amount: Union[int, None] = None,
+    nameorig: Union[str, None] = None,
+    namedest: Union[str, None] = None,
+    oldbalanceorg: Union[int, None] = None
 
 
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-data_path    = os.path.join(BASE_DIR, 'data', 'pay_sim.csv')
-model_path   = os.path.join(BASE_DIR, 'artifacts', 'model.pkl')
+@app.get('/prediction_form')
+def prediction_form():
+    return FileResponse('statics/index.html')
 
 
-best_model_info = load_object(os.path.join('artifacts','best_model_info.pkl'))
-print(best_model_info['threshold'])
+@app.post('/predict')
+def predict(data: PredictRequest):
+    try:
+        features = CustomData(
+        type = data.type,
+        amount = data.amount,
+        nameorig = data.nameorig,
+        namedest = data.namedest,
+        oldbalanceorg = data.oldbalanceorg)
+
+        df_features = features.get_data_as_dataframe() #converting into dataframe
+        df_features = feature_engineering_pipeline.feature_engineering(df_features)  #applying the feature engineering pipeline
+        result = predict_pipeline.predict(df_features)
+        
+        return result
+
+    except Exception as e:
+        raise CustomError(e,sys)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
