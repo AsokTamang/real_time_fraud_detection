0b56d06ac841f8483bee8f1e2e64ecab35808f05
AsokTamang <ashoktmg2058@gmail.com>
2026-05-29 11:57:01 -0400

    line chart of transaction per minute

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
 import streamlit as st
+import pandas as pd
+
 
 def display_ui():
     st.title("Real-Time Fraud Detection Dashboard")
@@ -32,7 +34,7 @@ def display_ui():
             icon="🚨",
         )        
     
-    #metric cards
+    #metric cards for showing the total number of transactions, fraud transactions, legit transactions and fraud rate percentage
     total       = st.session_state.total
     fraud_count = st.session_state.fraud_count
     legit_count = st.session_state.legit_count
@@ -44,5 +46,25 @@ def display_ui():
     m4.metric("📈 Fraud rate",          f"{fraud_rate:.1f}")
     
     st.divider()  
+    
+    chart_col1, chart_col2 = st.columns(2)
+    with chart_col1:
+        st.subheader("Transactions per minute")
+        buckets:dict = {}
+        if st.session_state.tpm_history:
+            for event in st.session_state.tpm_history:
+                buckets[event["time"]] = buckets.get(event["time"], 0) + event["count"]  #if the time label of the event doesnot exist in bucket, then we create new count for this current time label of event inside the bucket
+            df_tpm = pd.DataFrame(list(buckets.items()), columns=["time", "number of transactions"])
+            st.line_chart(df_tpm.set_index("time"))
+        else:
+            st.info("waiting for the incoming transactions...")
+            
+
+
+
+
+    
+
+
 
 
